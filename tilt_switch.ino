d81d2e72594bf0f41bfc135a105e7d4d10b5edfb
liauihui <33207264+liauihui@users.noreply.github.com>
2017-11-02 13:09:45 +0800

    Add files via upload
    3 e tongranx, zité si source file.

---
+++ tilt_switch.ino
@@ -0,0 +1,24 @@
+#include <SoftwareSerial.h>
+
+static const int tiltSwitchPin = 2;             // tiltSwitch Pin 
+static const int ledBoardPin =  LED_BUILTIN;    // built-in the LED pin
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledBoardPin, OUTPUT);
+  pinMode(tiltSwitchPin, INPUT);    // Signal_in
+}
+
+//=====================================
+void loop() {
+  
+  int tiltState = digitalRead(tiltSwitchPin);
+
+  if ( HIGH == tiltState ) {
+    digitalWrite(ledBoardPin, HIGH);  // Onboard LED
+  } else {
+    digitalWrite(ledBoardPin, LOW);
+  }
+
+}
+
